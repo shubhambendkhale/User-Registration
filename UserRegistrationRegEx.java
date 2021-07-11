71193fd1445064a6a6f32ebfc055f4cbd2675048
shubhambendkhale <shubhambendkhale90@gmail.com>
2021-07-11 12:07:07 +0530

    Added :SAMARTH@SAM MINGW64 ~/Desktop/UserRegistration (master) $ ls UserRegistrationRegEx.java  UserRegistrationTesting.java  gitignore
    SAMARTH@SAM MINGW64 ~/Desktop/UserRegistration (master)
    $ git add .
    
    SAMARTH@SAM MINGW64 ~/Desktop/UserRegistration (master)
    $ git commit -m Added

---
+++ UserRegistrationRegEx.java
@@ -0,0 +1,32 @@
+package com.Usergistration;
+
+import java.util.regex.Pattern;
+
+
+public class UserRegistrationRegEx {
+    private static final String NAME = "[A-Z][a-z]{2,}";
+    private static final String EMAIL = "^(abc)[.][a-z]+[@](bl)[.](co)[.][a-z]+$";
+    private static final String PHONE_NUMBER = "^[\\d]{2}[\s][\\d]{10}$";
+    private static final String PASSWORD = "^(?=.*[A-Z])(?=.*[a-z])(?=[^@#$%^&+=]*[@#$%^&+=][^@#$%^&+=]*$)(?=.*[0-9]).{8,}$";
+
+    public boolean firstNameValidate(String fName) {
+        return Pattern.matches(NAME, fName);
+    }
+
+    public boolean lastNameValidate(String lName) {
+        return Pattern.matches(NAME, lName);
+    }
+
+    public boolean emailValidate(String email) {
+        return Pattern.matches(EMAIL, email);
+    }
+
+    public boolean phoneNumberValidate(String phNum) {
+        return Pattern.matches(PHONE_NUMBER, phNum);
+    }
+
+    public boolean passwordValidate(String pw) {
+        return Pattern.matches(PASSWORD, pw);
+    }
+
+}
